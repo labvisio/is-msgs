9f2bba647af13410b8c1e245779e1ecc59cac8b3
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2017-10-25 15:44:49 -0200

    Improved usage. - Added cmake support - Removed sources - Added auto generated documentation

--- gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p js cpp py java
-protoc -I `pwd` --js_out=./js --cpp_out=./cpp --java_out=./java --python_out=./py *.proto 
-
--- install_cpp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo mkdir -p /usr/local/include/is /usr/local/include/is/msgs
-sudo cp -ir ./cpp/* /usr/local/include/is/msgs/ 
