bb8da851c6133abf7c59aae35dcddbb0fa6aa6bf
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2018-05-15 16:10:10 +0100

    fixes #1 - other .protos can now include the ones on this repo - increased cmake version (required for this change)

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 mkdir -p build
 cd build
-conan install .. -s compiler.libcxx=libstdc++11
+conan install .. -s compiler.libcxx=libstdc++11 --build=missing
 cmake -GNinja ..
-ninja
+ninja 
+if ninja help | grep -q "test:"; then
+  ninja test
+fi
