8adb0fba931e3096d9aa30aa94fee4352e558c44
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2018-06-07 15:23:40 +0100

    minor build system changes - now using conanfile.py - build works even if user does not use conan

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
+mkdir -p build && cd build
 conan install .. -s compiler.libcxx=libstdc++11 --build=missing
-cmake -GNinja ..
-ninja 
-if ninja help | grep -q "test:"; then
-  ninja test
-fi
+conan build ..
---
+++ conanfile.py
@@ -0,0 +1,38 @@
+from conans import ConanFile, CMake, tools
+
+
+class IsmsgsConan(ConanFile):
+    name = "is-msgs"
+    version = "1.1.4"
+    license = "MIT"
+    url = "https://github.com/labviros/is-msgs"
+    description = "Repository containing the schema for standard ::is messages"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"fPIC": [True, False], "build_tests": [True, False]}
+    default_options = "fPIC=True", "build_tests=True"
+    generators = "cmake", "cmake_find_package", "cmake_paths"
+    requires = ("protobuf/[>=3.0]@bincrafters/stable", "boost/[>=1.65]@conan/stable",
+                "spdlog/[>0.15]@bincrafters/stable")
+    exports_sources = "*"
+
+    def build_requirements(self):
+        if self.options.build_tests:
+            self.build_requires("opencv/[>=3.3]@is/stable")
+            self.build_requires("gtest/1.8.0@bincrafters/stable")
+
+    def configure(self):
+        self.options["opencv"].with_qt = False
+        self.options["spdlog"].fmt_external = False
+
+    def build(self):
+        cmake = CMake(self, generator='Ninja')
+        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        cmake.definitions["enable_tests"] = self.options.build_tests
+        cmake.configure()
+        cmake.build()
+        if self.options.build_tests:
+            cmake.test()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["is-msgs"]
