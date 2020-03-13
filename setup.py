a8dc35584d3f21ad526fdef516b272b8ef59176f
wagnercotta <waacotta@gmail.com>
2020-03-13 19:02:07 -0300

    renamed some fields and increased the number version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='is_msgs',
-    version='1.1.11',
+    version='1.1.12',
     description='Package with standard IS messages',
     url='http://github.com/labviros/is-msgs',
     author='labviros',
